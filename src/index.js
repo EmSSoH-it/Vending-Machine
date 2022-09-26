d8fd6626b0949a3aae7d581eee761097b529e6ca
EmSSoH-it <emssoh@hotmail.com>
2022-09-26 22:09:13 +0200

    vending machine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const drinks = [
+  {id: 0, label: 'Cola', value: 9},
+  {id: 1, label: 'Sprite', value: 5},
+  {id: 2, label: 'Fanta', value: 15}
+]
+
+class VendingMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {money: 0, drink: '', price: 0};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name
+    const drink = drinks[event.target.value]
+
+    if(name==="drink"){
+
+      this.setState({
+        drink: drink.label,
+        price: drink.value
+      })
+    } else {this.setState({[name]: event.target.value})};  
+  }
+  
+  handleSubmit(event) {
+    const change = (this.state.money - this.state.price);
+
+    this.setState({money: change});
+
+    alert(
+      'You have bought: ' + this.state.drink + 
+      ' \nremaining change: ' + change +
+      ' \nprice of drink was ' + this.state.price);
+    event.preventDefault();
+  }
+
+  
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}> 
+        <label>
+          Please input money:
+          <input name="money" type="number" value={this.state.money} onChange={this.handleChange} />
+        </label>
+        <label>
+          Please select your drink:
+          <select name="drink" defaultValue="" onChange={this.handleChange} >
+              <option hidden disabled value="">-- select an option --</option>
+            {drinks.map((option) => (
+              <option key={option.id} value={option.id}>{option.label} - {option.value}kr</option>
+            ))}
+          </select>
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<VendingMachine />);
